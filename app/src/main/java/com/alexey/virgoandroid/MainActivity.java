31984a6f9d5e7bcda0ccbe5719363728d31182fe
virgocode <>
2019-02-01 13:34:08 +0300

    add zxlib scanner and js bridge

--- app/src/main/java/com/alexey/virgoandroid/MainActivity.java
+++ app/src/main/java/com/alexey/virgoandroid/MainActivity.java
@@ -1,13 +1,95 @@
 package com.alexey.virgoandroid;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static Activity activity;
+    protected WebView webView;
+
+    //"file:///android_asset/index.html"
+    private final String LOCAL_FILE = "file:///android_asset/index.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        activity = this;
+
+
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            webSettings.setAllowFileAccessFromFileURLs(true);
+            webView.getSettings().setAllowFileAccessFromFileURLs(true);
+            webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        }
+
+        webView.setVerticalScrollBarEnabled(false);        // отключили прокрутку
+        webView.setHorizontalScrollBarEnabled(false);      // отключили прокрутку
+        webView.getSettings().setJavaScriptEnabled(true);  // включили JavaScript
+        webView.getSettings().setDomStorageEnabled(true);  // включили localStorage и т.п.
+        webView.getSettings().setSupportZoom(false);       // отключили зум, т.к. нормальные приложения подобным функционалом не обладают
+        webView.getSettings().setSupportMultipleWindows(false);   // отключили поддержку вкладок.
+        // Т.к. пользователь должен сидеть в SPA приложении
+        webView.addJavascriptInterface(new WebAppInterface(this), "_API");   // прокидываем объект в JavaScript.
+        webView.setWebChromeClient(new WebChromeClient() {
+            // Need to accept permissions to use the camera and audio
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+            }
+        });
+        webView.loadUrl(LOCAL_FILE);// загрузили нашу страничку
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result!=null){
+            if( result.getContents() == null){
+                //Toast.makeText(this, "RESULT: " + "cancel" , Toast.LENGTH_LONG).show();
+            }else {
+                String contents = result.getContents();
+                //webView.loadUrl("javascript:$('.test').text('"+contents+"')");
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
 }
---
+++ app/src/main/java/com/alexey/virgoandroid/WebAppInterface.java
@@ -0,0 +1,69 @@
+package com.alexey.virgoandroid;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class WebAppInterface extends MainActivity{
+    Context context;
+
+
+
+    /** Instantiate the interface and set the context */
+    public WebAppInterface(Context _context) {
+        context = _context;
+
+    }
+    /** Далее идут методы, которые появятся в JavaScript */
+    @JavascriptInterface
+    public void sendSms(String phoneNumber, String message) {
+        Log.e("WebAppInterface",phoneNumber);
+    }
+
+    @JavascriptInterface
+    public void log(String message) {
+        Log.e("WebAppInterface",message);
+    }
+
+
+    // This function can be called in our JS script now
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(context, toast, Toast.LENGTH_LONG).show();
+    }
+
+
+    @JavascriptInterface
+    public void openScanner() {
+        IntentIntegrator integrator = new IntentIntegrator( MainActivity.activity );
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setOrientationLocked(true);
+        integrator.setPrompt("");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
+    }
+
+    @JavascriptInterface
+    public void openScannerCamera() {
+        IntentIntegrator integrator = new IntentIntegrator( MainActivity.activity );
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setOrientationLocked(true);
+        integrator.setPrompt("");
+        integrator.setCameraId(1);
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
+    }
+
+}
+
+
